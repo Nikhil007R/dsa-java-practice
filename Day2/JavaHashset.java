21040b3bb40449c39bd2c2de0774477a67b38eb7
Nikhil Rawat <nikhilrawaa311@gmail.com>
2025-05-07 13:56:06 +0530

    [Nikhil] Add: added java hashset problem

---
+++ Day2/JavaHashset.java
@@ -0,0 +1,41 @@
+package Day2;
+
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class JavaHashset {
+
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        int t = s.nextInt();
+        String [] pair_left = new String[t];
+        String [] pair_right = new String[t];
+
+        for (int i = 0; i < t; i++) {
+            pair_left[i] = s.next();
+            pair_right[i] = s.next();
+        }
+
+
+        Set<String> hs = new HashSet<>();
+        for(int i=0; i<t; i++){
+
+            StringBuilder sb = new StringBuilder();
+            if(pair_left[i].compareTo(pair_right[i]) < 0){
+                sb.append(pair_left[i] + " ");
+                sb.append(pair_right[i]);
+            }
+            else{
+                sb.append(pair_right[i] + " ");
+                sb.append(pair_left[i]);
+            }
+            hs.add(sb.toString());
+
+            System.out.println(hs.size());
+        }
+
+    }
+}
